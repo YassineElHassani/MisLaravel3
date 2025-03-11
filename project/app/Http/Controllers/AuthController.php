5faf352df86fbfb80e1878a6a301d49ce0765b6a
Yassine El hassani <yassine.ds.404@gmail.com>
2025-03-11 11:08:18 +0000

    push

--- project/app/Http/Controllers/AuthController.php
+++ project/app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,5 +14,23 @@ class AuthController extends Controller
 
     public function login() {
         return view('login');
+    }
+
+    public function singin(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
     }
 }
